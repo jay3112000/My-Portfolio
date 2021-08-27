c1b975ab464a454356977e068a3eab00ec14a635
Jay Sharma <jayrocks3112000@gmail.com>
2021-08-27 10:33:17 -0700

    improved

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -16,54 +16,54 @@ function Carousel() {
       <p  className="d-flex justify-content-center">
             Examlples of my most Recent Work    
          </p>
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
       <a
       href='https://relaxed-visvesvaraya-22dec2.netlify.app'
       >
-      <img src={img} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
+      <img src={img} className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
         <h5>TRAVEL GUIDE</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <p>A guide to all your favourite destinations</p>
       </div>
       </a>
       
     </div>
-    <div class="carousel-item">
+    <div className="carousel-item">
       <a href='https://happy-bhaskara-3d76ea.netlify.app'>
-      <img src={img2} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
+      <img src={img2} className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
         <h5>MEME GENERATOR</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        <p>Simple create fun memes with friends</p>
       </div>
       </a>
      
     </div>
-    <div class="carousel-item">
+    <div className="carousel-item">
       <a
       href='https://vigilant-mahavira-5d35ee.netlify.app'>
-      <img src={img3} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
+      <img src={img3} className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
         <h5>KABANIZE</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+        <p>Boost your Productiveness with a simple technique</p>
       </div>
       </a>
       
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
   </button>
 </div>
 </div>
